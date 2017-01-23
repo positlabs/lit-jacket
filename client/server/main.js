1b13498677890776f57cffff28bc912094aa0266
Patrick Gunderson <patrick@toolofna.com>
2017-01-23 12:24:35 -0800

    fiddling with babel transform

--- client/server/main.js
+++ client/server/main.js
@@ -1,6 +1,6 @@
 require('babel-register')({
-	presets: ['preset-es2015'],
-	plugins: ['transform-runtime']
+	presets: ['es2015'],
+	// plugins: ['transform-runtime']
 });
 
 require('./js/test.js');
